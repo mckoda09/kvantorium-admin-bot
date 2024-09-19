e9d4886a7274ee7ccedae0f79530b5a544703e25
mckoda09 <mckoda09@gmail.com>
2024-09-19 14:48:16 +0000

    added dummy reaction

--- mod.ts
+++ mod.ts
@@ -3,5 +3,6 @@ import { Bot } from "grammy";
 export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
 
 bot.chatType("private").on("msg:text", async (ctx) => {
+    await ctx.react("👎");
     await ctx.reply(ctx.message.text);
 });
