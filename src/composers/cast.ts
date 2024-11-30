9477e93726cce5f0152a45b29522670936f01332
mckoda09 <mckoda09@gmail.com>
2024-11-30 14:35:12 +0500

    try catch in cast

--- src/composers/cast.ts
+++ src/composers/cast.ts
@@ -7,54 +7,56 @@ import { listCastMessages, setCastMessage } from "../db/message.ts";
 export const castComposer = new Composer();
 
 castComposer.chatType("supergroup").command("cast", async (c) => {
-  if (!await isCast(c.chat.id)) return await notCast(c);
+  if (!(await isCast(c.chat.id))) return await notCast(c);
 
   await setStatus(c.chat.id, "waitForCast");
   await c.reply("Отправьте сообщение для рассылки 👇\nДля отмены — /cancel");
 });
 
 castComposer.chatType("supergroup").on("msg:text", async (c) => {
-  if (await getStatus(c.chat.id) != "waitForCast") return;
-  if (!await isCast(c.chat.id)) return await notCast(c);
+  if ((await getStatus(c.chat.id)) != "waitForCast") return;
+  if (!(await isCast(c.chat.id))) return await notCast(c);
 
   await c.reply("Рассылка запущена...");
   await clearStatus(c.chat.id);
 
   const groups = await listCastGroups(c.chat.id);
-  let count = 0;
+  let success = 0;
+  let failed = 0;
 
   const sentId = crypto.randomUUID();
 
   for (const group of groups) {
-    const msg = await c.copyMessage(group);
-    await setCastMessage(c.chat.id, sentId, msg.message_id, group);
-    count++;
+    try {
+      const msg = await c.copyMessage(group);
+      await setCastMessage(c.chat.id, sentId, msg.message_id, group);
+      success++;
+    } catch {
+      failed++;
+    }
   }
 
   const reply_markup = new InlineKeyboard();
   reply_markup.text("Удалить", `remove-${sentId}`);
-  await c.reply(`Рассылка завершена! Групп: ${count}`, { reply_markup });
+  await c.reply(`Рассылка завершена! Групп: ${success}`, { reply_markup });
 });
 
-castComposer.chatType("supergroup").callbackQuery(
-  /^remove-.*/,
-  async (c) => {
-    if (!await isCast(c.chat.id)) return;
-    await c.editMessageReplyMarkup({ reply_markup: { inline_keyboard: [] } });
-    await c.answerCallbackQuery();
+castComposer.chatType("supergroup").callbackQuery(/^remove-.*/, async (c) => {
+  if (!(await isCast(c.chat.id))) return;
+  await c.editMessageReplyMarkup({ reply_markup: { inline_keyboard: [] } });
+  await c.answerCallbackQuery();
 
-    const sentMessages = await listCastMessages(
-      c.chat.id,
-      c.callbackQuery.data.split("-").slice(1).join("-") || "",
-    );
+  const sentMessages = await listCastMessages(
+    c.chat.id,
+    c.callbackQuery.data.split("-").slice(1).join("-") || "",
+  );
 
-    let count = 0;
+  let count = 0;
 
-    for (const message of sentMessages) {
-      await c.api.deleteMessage(message.groupId, message.messageId);
-      count++;
-    }
+  for (const message of sentMessages) {
+    await c.api.deleteMessage(message.groupId, message.messageId);
+    count++;
+  }
 
-    await c.reply(`Рассылка удалена! Групп: ${count}`);
-  },
-);
+  await c.reply(`Рассылка удалена! Групп: ${count}`);
+});
